4e2e8cd81e2cf348ebaf9311193d5555b9331e07
destineepierreartistry <destineepierreartistry@users.noreply.github.com>
2026-06-05 18:03:54 +0000

    deploy: ae004cb388e86167492eba221051e1f29a373d91

--- out/_next/static/G2Awmen_-2eMUd4tvComp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-94112a978f8057ee.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
